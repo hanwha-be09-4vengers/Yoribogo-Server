0ae388ac976f48ce77bfcef5a73192dcb3892f4b
changuk2 <changuk0308@naver.com>
2024-10-21 20:48:39 +0900

    Refactor: 젠킨스 빌드를 위한  테스트 코드제거

--- src/test/java/com/avengers/yoribogo/ServerApplicationTests.java
+++ src/test/java/com/avengers/yoribogo/ServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.avengers.yoribogo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServerApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.avengers.yoribogo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
